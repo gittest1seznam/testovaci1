b823f3485caf1c020942adecddcb0449c08a5a27
gittest1seznam <gittest1@seznam.cz>
2015-08-30 16:50:57 +0200

    nepridano nic

---
+++ index.php
@@ -0,0 +1,4 @@
+<h1>Lorem ipsum dolor</h1>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis dapibus nulla, in lacinia eros pharetra at. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse quis dui at elit pretium mattis id non erat.
+</p>
