12c0218fb62f5eef84b54f310d17461b57e43f8e
gittest1seznam <gittest1@seznam.cz>
2015-08-30 17:13:46 +0200

    zmena indexu php
    refrefreffr

--- index.php
+++ index.php
@@ -2,3 +2,5 @@
 <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis dapibus nulla, in lacinia eros pharetra at. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse quis dui at elit pretium mattis id non erat.
 </p>
+<p>ewdfwefwfqfgqwef
+</p>
